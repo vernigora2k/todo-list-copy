8cd593244f45af65b69b3929129bce80c786b76a
vernigora2k <58885741+vernigora2k@users.noreply.github.com>
2020-05-19 15:26:37 +0300

    v1.0.1stable 31:25 min

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import TodoList from './Todo/TodoList'
 
 function App() {
+  const todos = [
+    {id: 1, complited: false, title: 'Купить хлеб'},
+    {id: 1, complited: false, title: 'Купить масло'},
+    {id: 1, complited: false, title: 'Купить молоко'}
+  ]
+
   return (
-    <div></div>
+    <div className='wrapper'>
+      <h1>React Tutorial</h1>
+      <TodoList todos={todos}></TodoList>
+    </div>
   );
 }
 
